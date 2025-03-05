7761753e17366e709085fcb767fc75aba1af6a9a
liuyanqiu <liuyanqiu06@sina.com>
2025-03-05 10:42:07 +0800

    feat: 创建mcp-server
    feat: 更新readme
    
    feat: 更新readme
    
    fix: 修改api参数
    
    feat: 修改API_URL
    
    fix: 更新readme
    
    fix: 更新文档
    
    Create mcp-server,update readme

---
+++ README.md
@@ -0,0 +1,111 @@
+# supOS MCP Server
+
+本MCP服务器是基于Model Context Protocol (MCP)[https://modelcontextprotocol.io/introduction]协议提供的typescript-sdk进行开发，可以让任何支持MCP协议的客户端使用它。
+它提供了一系列supOS open-api的功能，例如：查询topic树结构，topic详情等。
+
+接下来跟随文档一起使用吧
+
+# 系统要求
+本服务基于typescript-sdk开发，需要具备Node.js环境
+
+
+## Tools
+
+1. `get-model-topic-tree`
+   - 查询topic 树结构菜单数据
+   - 输入:
+     - `key` (string): Fuzzy search keyword for child nodes
+     - `showRec` (string): Number of records to display
+     - `type` (string): Search type: 1--Text search, 2--Tag search
+   - 返回: topic 树结构菜单数据
+
+2. `get-model-topic-detail`
+   - 获取某个topic详情
+   - 输入:
+     - `topic` (string): The topic path corresponding to the model
+   - 返回: 某个topic详情
+
+## Setup
+
+### 获取apiKey
+登录[supOS社区版](https://supos-demo.supos.app/)，进入DataModeling菜单 -> 查看某个具体的topic详情，找到Data Operation -> Fetch，复制ApiKey
+
+### 使用 Claude Desktop
+这里以Claude Desktop App为例，想了解其他支持的客户端可访问[Model Context Protocol Client](https://modelcontextprotocol.io/clients)。
+
+添加以下内容到`claude_desktop_config.json`，可使用以下两种方式使用该服务:
+
+### 1.使用 mcp-server-supos
+ - NPX
+
+```json
+{
+  "mcpServers": {
+    "supos": {
+      "command": "npx",
+      "args": [
+        "-y",
+        "mcp-server-supos"
+      ],
+      "env": {
+        "SUPOS_API_KEY": "<API_KEY>",
+        "SUPOS_API_URL": "<API_URL>"
+      }
+    }
+  }
+}
+```
+
+- NODE
+
+由于Windows环境下`npx`执行可能有环境变量的问题，可以先本地安装`mcp-server-supos`包，再用`node`本地执行：
+
+```bash
+npm install mcp-server-supos -g
+```
+
+```json
+{
+  "mcpServers": {
+    "supos": {
+      "command": "node",
+      "args": [
+        "C://Users//xxx//AppData//Roaming//npm//node_modules//mcp-server-supos//dist//index.js"
+      ],
+      "env": {
+        "SUPOS_API_KEY": "<API_KEY>",
+        "SUPOS_API_URL": "<API_URL>"
+      }
+    }
+  }
+}
+```
+
+### 2.下载该项目本地执行
+
+Install
+```bash
+npm ci
+```
+
+Build
+```bash
+npm run build
+```
+
+```json
+{
+  "mcpServers": {
+    "supos": {
+      "command": "node",
+      "args": [
+        "<本地项目地址>//dist//index.js"
+      ],
+      "env": {
+        "SUPOS_API_KEY": "<API_KEY>",
+        "SUPOS_API_URL": "<API_URL>"
+      }
+    }
+  }
+}
+```
---
+++ src/index.ts
@@ -0,0 +1,154 @@
+#!/usr/bin/env node
+
+import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
+import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
+import fetch from "node-fetch";
+import { z } from "zod";
+import _ from "lodash";
+
+const server = new McpServer(
+  {
+    name: "mcp-server-supos",
+    version: "0.0.1",
+  },
+  {
+    capabilities: {
+      tools: {},
+    },
+  }
+);
+
+const SUPOS_API_KEY =
+  process.env.SUPOS_API_KEY;
+const SUPOS_API_URL =
+  process.env.SUPOS_API_URL;
+
+if (!SUPOS_API_KEY) {
+  console.error("SUPOS_API_KEY environment variable is not set");
+  process.exit(1);
+}
+
+type TreeNode = {
+  name: string;
+  countChildren: number;
+  path: string;
+  children: TreeNode[];
+};
+
+async function getModelTopicDetail(topic: string): Promise<any> {
+  const url = `${SUPOS_API_URL}/open-api/supos/uns/model?topic=${encodeURIComponent(
+    topic
+  )}`;
+
+  const response = await fetch(url, {
+    headers: {
+      apiKey: `${SUPOS_API_KEY}`,
+    },
+  });
+
+  if (!response.ok) {
+    throw new Error(`SupOS API error: ${response.statusText}`);
+  }
+
+  return await response.json();
+}
+
+async function getModelTopicTree(params: {
+  key?: string;
+  showRec?: string;
+  type?: string;
+}): Promise<any> {
+  let searchParams = {};
+  if (params) {
+    searchParams = _.pickBy(params, !_.isNil);
+  }
+  const paramsUrl = new URLSearchParams("");
+  _.forOwn(searchParams, function (value, key) {
+    paramsUrl.append(key, encodeURIComponent(value));
+  });
+
+  const url = `${SUPOS_API_URL}/open-api/supos/uns/tree?${paramsUrl.toString()}`;
+
+  const response = await fetch(url, {
+    headers: {
+      apiKey: `${SUPOS_API_KEY}`,
+    },
+  });
+
+  if (!response.ok) {
+    throw new Error(`SupOS API error: ${response.statusText}`);
+  }
+
+  return await response.json();
+}
+
+// server.prompt("Show all topic", async () => {
+//   const treeNotes: TreeNode =
+//     (await getModelTopicTree({ key: "" }))?.data || {};
+//   return {
+//     messages: [
+//       {
+//         role: "user",
+//         content: {
+//           type: "text",
+//           text: JSON.stringify(treeNotes),
+//         },
+//       },
+//       {
+//         role: "user",
+//         content: {
+//           type: "text",
+//           text: "请根据以上数据生成json文件,且支持下载,并将数据可视化展示",
+//         },
+//       },
+//     ],
+//   };
+//   // return {
+//   //   prompts: [
+//   //     {
+//   //       name: "summarize_treeNotes",
+//   //       description: "获取树节点资源",
+//   //     }
+//   //   ]
+//   // };
+// });
+
+server.tool(
+  "get-model-topic-tree",
+  {
+    key: z.string().optional().describe("Fuzzy search keyword for child nodes"),
+    showRec: z.boolean().optional().describe("Is show recommend topic"),
+    type: z
+      .string()
+      .optional()
+      .describe("Search type: 1--Text search, 2--Tag search"),
+  },
+  async (args) => {
+    const trees = await getModelTopicTree({
+      key: _.isNil(args.key) ? "" : `${args.key}`,
+      showRec: _.isNil(args.showRec) ? "false" : `${args.showRec}`,
+      type: _.isNil(args.type) ? "1" : `${args.type}`,
+    });
+    return {
+      content: [{ type: "text", text: JSON.stringify(trees) }],
+    };
+  }
+);
+
+server.tool("get-model-topic-detail", {topic: z.string()}, async (args) => {
+  const detail = await getModelTopicDetail(args.topic);
+  return {
+    content: [{ type: "text", text: `${JSON.stringify(detail)}` }],
+  };
+});
+
+async function runServer() {
+  const transport = new StdioServerTransport();
+  await server.connect(transport);
+  console.error("SupOS MCP Server running on stdio");
+}
+
+runServer().catch((error) => {
+  console.error("Fatal error in main():", error);
+  process.exit(1);
+});
